09ee10365f938ccbb3c3426f22567b9aaa95ed2f
wdfhai <wd.fhai@outlook.com>
2021-02-14 16:21:12 -0600

    refactored into Avengers Assemble game

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,25 @@
 
-const tableData = require('../db/tables.json');
-const waitListData = require('../db/wait.json');
+const teamData = require('../db/team.json');
+const appsListData = require('../db/apps.json');
 
 
 module.exports = (app) => {
-  app.get('/api/tables', (req, res) => res.json(tableData));
-  app.get('/api/wait', (req, res) => res.json(waitListData));
+  app.get('/api/team', (req, res) => res.json(teamData));
+  app.get('/api/apps', (req, res) => res.json(appsListData));
 
-  app.post('/api/tables', (req, res) => {
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+  app.post('/api/team', (req, res) => {
+    if (teamData.length < 5) {
+      teamData.push(req.body);
       res.json(true);
     } else {
-      waitListData.push(req.body);
+      appsListData.push(req.body);
       res.json(false);
     }
   });
 
   app.post('/api/clear', (req, res) => {
-    tableData.length = 0;
-    waitListData.length = 0;
+    teamData.length = 0;
+    appsListData.length = 0;
 
     res.json({ ok: true });
   });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 
 module.exports = (app) => {
-  app.get('/tables', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/tables.html'));
+  app.get('/team', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/team.html'));
   });
 
-  app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/reservations.html'));
+  app.get('/applications', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/applications.html'));
   });
 
   app.get('*', (req, res) => {
