7b2e2b821e5155acfb145883ec79b77bdf474c24
wdfhai <wd.fhai@outlook.com>
2021-02-14 16:48:20 -0600

    added images

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/assets', express.static(__dirname + '/assets'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
